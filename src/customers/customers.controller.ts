b368b015ee92402583950cccd437b00ead5359bb
Курочкин Кирилл <kirikura.w@yandex.ru>
2023-05-16 12:46:40 +0300

    Added ApiTags/Properties for customer entity

--- src/customers/customers.controller.ts
+++ src/customers/customers.controller.ts
@@ -2,36 +2,44 @@ import { Controller, Post, Body, Param, Get, Put, Delete } from '@nestjs/common'
 import { CustomersService } from './customers.service';
 import { CreateCustomerDto, UpdateCustomerDto, PartialCustomerDto } from './customers.dto';
 import { Customer } from './customers.entity';
+import { ApiTags, ApiOperation } from '@nestjs/swagger';
 
+@ApiTags('Покупатели') // Тег для документации
 @Controller('customers')
 export class CustomersController {
   constructor(private readonly customersService: CustomersService) {}
 
+  @ApiOperation({ summary: 'Создание покупателя' }) // Операция для Swagger
   @Post()
   createCustomer(@Body() createCustomerDto: CreateCustomerDto) {
     return this.customersService.createCustomer(createCustomerDto);
   }
 
+  @ApiOperation({ summary: 'Получение всех покупателей' }) // Операция для Swagger
   @Get()
   getAllCustomers() {
     return this.customersService.getAllCustomers();
   }
 
+  @ApiOperation({ summary: 'Получение покупателя по ID' }) // Операция для Swagger
   @Get(':id')
   getCustomerById(@Param('id') id: string) {
     return this.customersService.getCustomerById(Number(id));
   }
 
+  @ApiOperation({ summary: 'Обновление покупателя по ID' }) // Операция для Swagger
   @Put(':id')
   updateCustomer(@Param('id') id: string, @Body() updateCustomerDto: UpdateCustomerDto) {
     return this.customersService.updateCustomer(Number(id), updateCustomerDto);
   }
 
+  @ApiOperation({ summary: 'Удаление покупателя по ID' }) // Операция для Swagger
   @Delete(':id')
   deleteCustomer(@Param('id') id: string) {
     return this.customersService.deleteCustomer(Number(id));
   }
 
+  @ApiOperation({ summary: 'Получение частичную инфу покупателя по ID' }) // Операция для Swagger
   @Get(':id/partial')
   getCustomerPartialInfo(@Param('id') id: string) {
     return this.customersService.getCustomerPartialInfo(Number(id));
--- src/customers/customers.entity.ts
+++ src/customers/customers.entity.ts
@@ -1,26 +1,34 @@
 import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from 'typeorm';
 import { Order } from '../orders/orders.entity';
+import { ApiTags, ApiProperty } from '@nestjs/swagger';
 
-@Entity('customers')
+@Entity('customers')//указываем что это не просто клас, а сущность в рамках TypeOrm, в БД будет храниться как таблица
 export class Customer {
+  @ApiProperty({ example: '1', description: 'Уникальный идентификатор' })
   @PrimaryGeneratedColumn()
   customer_id: number;
 
+  @ApiProperty({ example: 'Иванов Иван Иванович', description: 'ФИО' })
   @Column()
   customer_name: string;
 
+  @ApiProperty({ example: 'ivavava@ya.ru', description: 'Почта' })
   @Column()
   customer_email: string;
 
+  @ApiProperty({ example: '89153333333', description: 'Телефон' })
   @Column()
   customer_phone: string;
 
+  @ApiProperty({ example: 'М', description: 'Пол' })
   @Column()
   customer_gender: string;
 
+  @ApiProperty({ example: '17', description: 'Возраст' })
   @Column()
   customer_age: number;
 
+  @ApiProperty({ example: 'РФ', description: 'Гражданство' })
   @Column()
   customer_nationality: string;
 
